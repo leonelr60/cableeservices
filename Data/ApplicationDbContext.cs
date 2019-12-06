2e99f9e92a5b828abcbe76dc7230ed37e113a4f5
Edgar Ramirez <leonelr60@hotmail.com>
2019-12-06 02:16:51 -0600

    Update ApplicationDbContext.cs, SQLContext.cs, and 4 more files...

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace cableeservices.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public readonly IHttpContextAccessor _httpContextAccessor;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
             : base(options)
         {
+            _httpContextAccessor = httpContextAccessor;
         }
     }
 }
---
+++ Data/SQLContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace cableeservices.Data
+{
+    public class SQLContext : DbContext
+    {
+        //public DbSet<AspNetUsers> AspNetUsers { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            try
+            {
+                string dbConn = "Data Source=sql5041.site4now.net;Initial Catalog=DB_A4AF65_ecableservices;User ID=DB_A4AF65_ecableservices_admin;Password=S1sr4m*.;MultipleActiveResultSets=true";
+                optionsBuilder.UseSqlServer(@dbConn);
+            } catch(Exception ex)
+            {
+                string sError = ex.Message;
+
+            }
+
+            
+        }
+    }
+}
